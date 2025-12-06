41269565f51aaece41070f1bfd273dabda824906
Hossam Eldin Mahmoud <hossameldinmi@gmail.com>
2025-12-06 22:39:25 +0200

    [Feature] add lint_ignore config

--- lib/src/v2/shared/file_data.dart
+++ lib/src/v2/shared/file_data.dart
@@ -1,3 +1,5 @@
+import 'package:collection/collection.dart';
+import 'package:dart_style/dart_style.dart';
 import 'package:equatable/equatable.dart';
 import 'package:i69n/src/v2/shared/file.dart';
 import 'package:i69n/src/v2/shared/file_metadata.dart';
@@ -7,7 +9,8 @@ class FileData extends Equatable {
   final FileMetadata metadata;
   final List<Node> nodes;
   final List<Import> imports;
-  FileData(this.metadata, this.nodes, this.imports);
+  final List<String> lintIgnore;
+  FileData(this.metadata, this.nodes, this.imports, this.lintIgnore);
 
   factory FileData.parseMap(String filePath, Map<dynamic, dynamic> map) {
     final file = LocaleFile(filePath);
@@ -15,7 +18,8 @@ class FileData extends Equatable {
     final configNodes = _getConfigNodes(nodes);
     final imports = _getImports(configNodes);
     final fileMetadata = FileMetadata.fromData(configNodes, file);
-    return FileData(fileMetadata, nodes, imports);
+    final ignores = _getIgnores(configNodes);
+    return FileData(fileMetadata, nodes, imports, ignores);
   }
 
   static List<ConfigNode> _getConfigNodes(Iterable<Node> nodes) {
@@ -32,17 +36,41 @@ class FileData extends Equatable {
     return [];
   }
 
+  static List<String> _getIgnores(List<ConfigNode> nodes) {
+    if (nodes.isEmpty) {
+      return [];
+    }
+    final ignoreNode = nodes.firstWhereOrNull((e) => e.hasFlag('lint_ignore'));
+    if (ignoreNode == null) return [];
+    return ignoreNode.value.value.map((e) => e).toList();
+  }
+
   String build() {
-    return <String>[
-      '// ignore_for_file: unused_element, unused_field, camel_case_types, annotate_overrides, prefer_single_quotes',
-      '// GENERATED FILE, do not edit!',
-      "import 'package:i69n/i69n.dart' as i69n;",
-      imports.map((e) => "import '$e';").join('\n'),
-      "",
-      "String get _languageCode => '${metadata.languageCode}';",
-      "String get _localeName => '${metadata.localeName}';",
-      "",
-    ].join('\n');
+    final output = StringBuffer();
+    output.write(
+        '// ignore_for_file: unused_element, unused_field, camel_case_types, annotate_overrides, prefer_single_quotes');
+    if (lintIgnore.isNotEmpty) {
+      output.write(', ');
+      output.writeAll(lintIgnore);
+    }
+    output.writeln('');
+    output.writeln('// GENERATED FILE, do not edit!');
+    output.writeln("import 'package:i69n/i69n.dart' as i69n;");
+    imports.map((e) => "import '$e';").forEach((e) => output.writeln(e));
+    output.writeln('');
+    output.writeln("String get _languageCode => '${metadata.languageCode}';");
+    output.writeln("String get _localeName => '${metadata.localeName}';");
+    output.writeln('');
+    try {
+      var formatter = DartFormatter(
+        languageVersion: DartFormatter.latestShortStyleLanguageVersion,
+      );
+      return formatter.format(output.toString());
+    } catch (e) {
+      print(
+          'Cannot format ${metadata.languageCode}, ${metadata.localeFile.filePath} messages. You might need to escape some special characters with a backslash. Please investigate generated class.');
+      return output.toString();
+    }
   }
 
   @override
--- test/parsing_test.dart
+++ test/parsing_test.dart
@@ -10,80 +10,85 @@ import 'mock/fixture.dart';
 void main() {
   test('testMessages.i69n', () async {
     await Fixture.testParsing('testMessages', (filePath, actual) async {
-      final expected = FileData(FileMetadata(LocaleFile(filePath), true, 'en', 'sk'), [
-        ConfigNode(StringNodeKey('_i69n_import'), StringListNodeValue(['dart:io'])),
-        ConfigNode(StringNodeKey('_i69n_language'), StringListNodeValue(['sk'])),
-        Node(
-          StringNodeKey('generic'),
-          NodeListNodeValue([
-            ConfigNode(StringNodeKey('_i69n'), StringListNodeValue(['flag'])),
-            Node(StringNodeKey('ok'), StringNodeValue('OK')),
-            Node(StringNodeKey('done'), StringNodeValue('DONE')),
-            Node(StringNodeKey('letsGo'), StringNodeValue('Let\'s go!')),
-            Node(ParametrizedNodeKey('ordinalNumber', [Parameter('n', 'int')]),
-                GrammaticalNumberNodeValue("\${_ordinal(n, one: '1st', two: '2nd', few: '3rd', other: '\${n}th')}")),
-          ]),
-        ),
-        Node(
-          StringNodeKey('invoice'),
-          NodeListNodeValue([
-            ConfigNode(StringNodeKey('_i69n'), StringListNodeValue(['noescape', 'nomap'])),
-            Node(StringNodeKey('create'), StringNodeValue('Create invoice')),
-            Node(StringNodeKey('delete'), StringNodeValue('Delete  invoice')),
-            Node(StringNodeKey('help'),
-                StringNodeValue('Use this function to generate new invoices and stuff. Awesome!')),
-            Node(
-              ParametrizedNodeKey('count', [Parameter('cnt', 'int')]),
-              GrammaticalNumberNodeValue("You have created \$cnt \${_plural(cnt, one:'invoice', many:'invoices')}."),
-            ),
-            Node(StringNodeKey('something'), StringNodeValue(r"Let\'s go!")),
-          ]),
-        ),
-        Node(
-          StringNodeKey('apples'),
-          NodeListNodeValue(
-            [
-              Node(
-                ParametrizedNodeKey('_apples', [Parameter('cnt', 'int')]),
-                GrammaticalNumberNodeValue(
-                    "\${_plural(cnt, zero: 'no apples', one:'\$cnt apple', many:'\$cnt apples')}"),
-              ),
+      final expected = FileData(
+        FileMetadata(LocaleFile(filePath), true, 'en', 'sk'),
+        [
+          ConfigNode(StringNodeKey('_i69n_import'), StringListNodeValue(['dart:io'])),
+          ConfigNode(StringNodeKey('_i69n_language'), StringListNodeValue(['sk'])),
+          Node(
+            StringNodeKey('generic'),
+            NodeListNodeValue([
+              ConfigNode(StringNodeKey('_i69n'), StringListNodeValue(['flag'])),
+              Node(StringNodeKey('ok'), StringNodeValue('OK')),
+              Node(StringNodeKey('done'), StringNodeValue('DONE')),
+              Node(StringNodeKey('letsGo'), StringNodeValue('Let\'s go!')),
+              Node(ParametrizedNodeKey('ordinalNumber', [Parameter('n', 'int')]),
+                  GrammaticalNumberNodeValue("\${_ordinal(n, one: '1st', two: '2nd', few: '3rd', other: '\${n}th')}")),
+            ]),
+          ),
+          Node(
+            StringNodeKey('invoice'),
+            NodeListNodeValue([
+              ConfigNode(StringNodeKey('_i69n'), StringListNodeValue(['noescape', 'nomap'])),
+              Node(StringNodeKey('create'), StringNodeValue('Create invoice')),
+              Node(StringNodeKey('delete'), StringNodeValue('Delete  invoice')),
+              Node(StringNodeKey('help'),
+                  StringNodeValue('Use this function to generate new invoices and stuff. Awesome!')),
               Node(
                 ParametrizedNodeKey('count', [Parameter('cnt', 'int')]),
-                StringNodeValue("You have eaten \${_apples(cnt)}."),
+                GrammaticalNumberNodeValue("You have created \$cnt \${_plural(cnt, one:'invoice', many:'invoices')}."),
               ),
-              Node(
-                ParametrizedNodeKey('problematic', [Parameter('count', 'int')]),
-                GrammaticalNumberNodeValue(
-                    "\${_plural(count, zero:'didn\\'t find any tasks', one:'found 1 task', other: 'found \$count tasks')}"),
-              ),
-              Node(StringNodeKey('anotherProblem'), StringNodeValue('here\nthere')),
-              Node(StringNodeKey('quotes'), StringNodeValue('Hello \\\"world\\\"!')),
-              Node(StringNodeKey('quotes2'), StringNodeValue('Hello \\"world\\"!')),
-            ],
+              Node(StringNodeKey('something'), StringNodeValue(r"Let\'s go!")),
+            ]),
           ),
-        ),
-        Node(
-          StringNodeKey('friends'),
-          NodeListNodeValue([
-            Node(
-                StringNodeKey('michael'),
-                NodeListNodeValue([
-                  Node(StringNodeKey('name'), StringNodeValue('Aaaaa')),
-                  Node(StringNodeKey('description'), StringNodeValue('Aa Aa Aa')),
-                ])),
-            Node(
-                StringNodeKey('eva'),
-                NodeListNodeValue([
-                  ConfigNode(StringNodeKey('_i69n_implements'), StringListNodeValue(['MichaelFriendsTestMessages'])),
-                  Node(StringNodeKey('name'), StringNodeValue('Bbbbb')),
-                  Node(StringNodeKey('description'), StringNodeValue('Bb Bb Bb')),
-                ])),
-          ]),
-        ),
-      ], [
-        Import('dart:io')
-      ]);
+          Node(
+            StringNodeKey('apples'),
+            NodeListNodeValue(
+              [
+                Node(
+                  ParametrizedNodeKey('_apples', [Parameter('cnt', 'int')]),
+                  GrammaticalNumberNodeValue(
+                      "\${_plural(cnt, zero: 'no apples', one:'\$cnt apple', many:'\$cnt apples')}"),
+                ),
+                Node(
+                  ParametrizedNodeKey('count', [Parameter('cnt', 'int')]),
+                  StringNodeValue("You have eaten \${_apples(cnt)}."),
+                ),
+                Node(
+                  ParametrizedNodeKey('problematic', [Parameter('count', 'int')]),
+                  GrammaticalNumberNodeValue(
+                      "\${_plural(count, zero:'didn\\'t find any tasks', one:'found 1 task', other: 'found \$count tasks')}"),
+                ),
+                Node(StringNodeKey('anotherProblem'), StringNodeValue('here\nthere')),
+                Node(StringNodeKey('quotes'), StringNodeValue('Hello \\\"world\\\"!')),
+                Node(StringNodeKey('quotes2'), StringNodeValue('Hello \\"world\\"!')),
+              ],
+            ),
+          ),
+          Node(
+            StringNodeKey('friends'),
+            NodeListNodeValue([
+              Node(
+                  StringNodeKey('michael'),
+                  NodeListNodeValue([
+                    Node(StringNodeKey('name'), StringNodeValue('Aaaaa')),
+                    Node(StringNodeKey('description'), StringNodeValue('Aa Aa Aa')),
+                  ])),
+              Node(
+                  StringNodeKey('eva'),
+                  NodeListNodeValue([
+                    ConfigNode(StringNodeKey('_i69n_implements'), StringListNodeValue(['MichaelFriendsTestMessages'])),
+                    Node(StringNodeKey('name'), StringNodeValue('Bbbbb')),
+                    Node(StringNodeKey('description'), StringNodeValue('Bb Bb Bb')),
+                  ])),
+            ]),
+          ),
+        ],
+        [
+          Import('dart:io'),
+        ],
+        [],
+      );
       ;
       expect(actual.imports, expected.imports);
       expect(actual.metadata, expected.metadata);
